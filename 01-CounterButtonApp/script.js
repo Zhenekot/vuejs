85ff805611c088ed62ebc81bb03f3b5579456097
Zhenekot <105406228+Zhenekot@users.noreply.github.com>
2023-05-16 19:09:37 +0500

    Add files via upload

---
+++ 01-CounterButtonApp/script.js
@@ -0,0 +1,12 @@
+ import { createApp } from './vendor/vue.esm-browser.js';
+
+// Создайте Vue приложение
+
+createApp({
+    data() {
+      return {
+        count: 0
+      }
+    }
+  }).mount('#app')
+
