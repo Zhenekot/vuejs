98559dae2c90323c9cd7986ea3374be6e0d99eab
Zhenekot <105406228+Zhenekot@users.noreply.github.com>
2023-05-21 20:53:40 +0500

    Add files via upload

---
+++ 01-CounterButtonComponent/CounterButton.js
@@ -0,0 +1,14 @@
+import { defineComponent } from './vendor/vue.esm-browser.js';
+
+export default defineComponent({
+  name: 'CounterButton',
+
+  // Компонент должен иметь входной параметр и порождать событие
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+  template: `<button type="button" @click="$emit('update:count', count+1)">{{count}}</button>`,
+});
